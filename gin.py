5c1edf910f5d9552845e8fe95c3e4b3b5353379d
vedugpt77 <54090649+vedugpt77@users.noreply.github.com>
2021-11-03 12:55:08 +0530

    Add files via upload

---
+++ gin.py
@@ -0,0 +1,17 @@
+from gingerit.gingerit import GingerIt
+import streamlit as st
+
+st.title('Grammar & Spell Checker')
+text = st.text_area("Enter Text:", value='', height=None, max_chars=None, key=None)
+parser = GingerIt()
+if st.button('Correct Sentence'):
+    if text == '':
+        st.write('Please enter text for checking') 
+    else:
+        try: 
+            result_dict = parser.parse(text)
+            st.markdown('**Corrected Sentence - **' + str(result_dict["result"]))
+        except:
+            st.write('Please check internet connectivity')
+else:
+     pass
